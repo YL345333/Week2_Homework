777a9b5a297a5de1450778fd7395528b8ea9d0e0
YL345333 <yangliu@lcad.edu>
2025-09-06 02:50:45 -0700

    Goal Script

--- Assignment2/Assets/Scenes/Scene 1/Playermovement.cs
+++ Assignment2/Assets/Scenes/Scene 1/Playermovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerscriped : MonoBehaviour
+public class Playermovement: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
